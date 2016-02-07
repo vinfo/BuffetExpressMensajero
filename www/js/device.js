672416389fb3a51729c823d0056dac24e9716876
VICTOR VALENCIA <victor.valencia@gmail.com>
2016-02-06 23:57:47 -0500

    Test Background

--- www/js/device.js
+++ www/js/device.js
@@ -41,8 +41,85 @@
         //navigator.splashscreen.show();
         checkConnection();
         getDeviceProperty();
-        navigator.splashscreen.hide();    
-    }
+        navigator.splashscreen.hide();
+        setBackground();
+        if (window.plugins.backgroundGeoLocation) {
+            app.configureBackgroundGeoLocation();
+        }        
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+    configureBackgroundGeoLocation: function() {
+        // Your app must execute AT LEAST ONE call for the current position via standard Cordova geolocation,
+        //  in order to prompt the user for Location permission.
+        window.navigator.geolocation.getCurrentPosition(function(location) {
+            console.log('Location from Phonegap');
+        });
+
+        var bgGeo = window.plugins.backgroundGeoLocation;
+
+        /**
+        * This would be your own callback for Ajax-requests after POSTing background geolocation to your server.
+        */
+        var yourAjaxCallback = function(response) {
+            ////
+            // IMPORTANT:  You must execute the #finish method here to inform the native plugin that you're finished,
+            //  and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+            // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+            //
+            //
+            bgGeo.finish();
+        };
+
+        /**
+        * This callback will be executed every time a geolocation is recorded in the background.
+        */
+        var callbackFn = function(location) {
+            console.log('[js] BackgroundGeoLocation callback:  ' + location.latitudue + ',' + location.longitude);
+            // Do your HTTP request here to POST location to your server.
+            //
+            //
+            yourAjaxCallback.call(this);
+        };
+
+        var failureFn = function(error) {
+            console.log('BackgroundGeoLocation error');
+        }
+        
+        // BackgroundGeoLocation is highly configurable.
+        bgGeo.configure(callbackFn, failureFn, {
+            url: 'http://only.for.android.com/update_location.json', // <-- only required for Android; ios allows javascript callbacks for your http
+            params: {                                               // HTTP POST params sent to your server when persisting locations.
+                auth_token: 'user_secret_auth_token',
+                foo: 'bar'
+            },
+            headers: {
+                'X-Foo': 'bar'
+            },
+            desiredAccuracy: 10,
+            stationaryRadius: 20,
+            distanceFilter: 30,
+            notificationTitle: 'Background tracking',   // <-- android only, customize the title of the notification
+            notificationText: 'ENABLED',                // <-- android only, customize the text of the notification
+            activityType: "AutomotiveNavigation",       // <-- iOS-only
+            debug: true     // <-- enable this hear sounds for background-geolocation life-cycle.
+        });
+
+        // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
+        bgGeo.start();
+
+        // If you wish to turn OFF background-tracking, call the #stop method.
+        // bgGeo.stop()
+    }    
 };
 
 function checkConnection() {
@@ -86,4 +163,24 @@ function getDeviceProperty()
    var sPath = window.location.pathname;
    var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
    return sPage;
-} 
+}
+function setBackground(){
+        var BGN = window.plugins.backgroundNotification;
+
+        // Your background-fetch handler.
+        var notificationCallback = function(notification) {
+            console.log('BackgroundNotification received');
+
+            // HTTP to the server.
+            $.get({
+                url: '/heartbeat.json',
+                callback: function(response) {
+                    // Inform native plugin that we're finished here and the bg-thread can be finished.
+                    // Don't forget or OS will probably kill your app for bad behaviour.
+                    alert("Res:" + JSON.stringify(response));
+                    BGN.finish();
+                }
+            });
+        }
+        BGN.configure(notificationCallback);
+}
