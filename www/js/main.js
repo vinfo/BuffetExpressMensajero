f374159bc4a92092e0a6808346d2811c9007e0d3
VICTOR VALENCIA <victor.valencia@gmail.com>
2016-06-16 09:42:10 -0500

    Cambios Programacion

--- www/js/main.js
+++ www/js/main.js
@@ -57,33 +57,35 @@
 
 	angularRoutingApp.controller('ordenesController', function($scope,$location,$interval){		
 		$(".links").attr("href","");					
-		getRoutes();						
-		var timer= $interval(function(){
-			if(localStorage.num_ordenes && !localStorage.flagScreen){
-				var num_orders= JSON.parse(localStorage.num_ordenes);
-				var getOrders = ajaxrest.getOrders();
-				if(getOrders.length>0 && (getOrders.length != num_orders.num))getRoutes();	
-			}else{
-				localStorage.setItem("num_ordenes",JSON.stringify({route:0,num:0}));
-			}
-			
-			getPosition();
-			
-			try {
-				var pos1= JSON.parse(localStorage.position);
-				var pos2= JSON.parse(localStorage.position2);
-				var p1= pos1["lat"].toString().substring(0,9);
-				var p2= pos2["lat"].toString().substring(0,9);
-				if(p1 !== p2){
-					if(localStorage.position2)localStorage.setItem("position",localStorage.position2);
-					new Maplace().CenterMap();
-					getRoutes();							
+		getRoutes();
+		if ( $("#lordenes").length > 0 ) {
+			var timer= $interval(function(){
+				if(localStorage.num_ordenes && !localStorage.flagScreen){
+					var num_orders= JSON.parse(localStorage.num_ordenes);
+					var getOrders = ajaxrest.getOrders();
+					if(getOrders.length>0 && (getOrders.length != num_orders.num))getRoutes();	
+				}else{
+					localStorage.setItem("num_ordenes",JSON.stringify({route:0,num:0}));
 				}
-			}
-			catch(err) {
-			    console.log(err.message);
-			}
-		},15000);
+				
+				getPosition();
+				
+				try {
+					var pos1= JSON.parse(localStorage.position);
+					var pos2= JSON.parse(localStorage.position2);
+					var p1= pos1["lat"].toString().substring(0,9);
+					var p2= pos2["lat"].toString().substring(0,9);
+					if(p1 !== p2){
+						if(localStorage.position2)localStorage.setItem("position",localStorage.position2);
+						new Maplace().CenterMap();
+						getRoutes();							
+					}
+				}
+				catch(err) {
+				    console.log(err.message);
+				}
+			},15000);
+		}
 		$(".pedidotar").css({"bottom":$(".menupie").height()+"px"});	
 		localStorage.setItem("request","true");
 		//setTimeout(function(){ getOrdens(); }, 5000);	
