1266adb770f4d8c504915b1145e4bd0c3744c2bc
VICTOR VALENCIA <victor.valencia@gmail.com>
2016-07-11 15:31:21 -0500

    direct

--- www/js/internal.js
+++ www/js/internal.js
@@ -3,7 +3,22 @@ var id, target, option;
 if (window.jQuery) {  
  $(function(){
     //Establecer tracking
-    ajaxrest.getServerPosition();
+    setInterval(function(){
+      $.ajax({
+         type: 'GET',
+         url: 'http://buffetexpress.co/coordinates.php',
+         crossDomain: true,
+         data: 'coordinates=true',
+         dataType: 'json',
+         async: false, 
+         success: function(msg) {
+           alert(msg);
+         },
+        error: function (request, status, error) {
+          console.log("Error registro coordenadas server: "+ status);
+        }
+       });
+     }, 15000);
     
     // Tamaño container  
     $(".container").css({"min-height":$(document).height()});
