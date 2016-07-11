1093ea366f2b2af6cf73c7a60e258d595f5cadfb
VICTOR VALENCIA <victor.valencia@gmail.com>
2016-07-11 15:18:44 -0500

    js

--- www/js/internal.js
+++ www/js/internal.js
@@ -3,41 +3,7 @@ var id, target, option;
 if (window.jQuery) {  
  $(function(){
     //Establecer tracking
-    window.navigator.geolocation.getCurrentPosition(function(location) {
-        console.log('Location from Phonegap');
-    });
-    var bgGeo = window.plugins.backgroundGeoLocation;
-    var yourAjaxCallback = function(response) {
-        console.log("Finish");
-        bgGeo.finish();
-    };    
-    var callbackFn = function(location) {
-        console.log('[js] BackgroundGeoLocation callback:  ' + location.latitude + ',' + location.longitude);
-        yourAjaxCallback.call(this);
-    };
-    var failureFn = function(error) {
-        console.log('BackgroundGeoLocation error');
-    }
-    bgGeo.configure(callbackFn, failureFn, {
-        url: 'http://buffetexpress.co/REST/resources/printer/composer.json', // <-- Android ONLY:  your server url to send locations to
-        params: {
-            auth_token: 'user_secret_auth_token',    //  <-- Android ONLY:  HTTP POST params sent to your server when persisting locations.
-            foo: 'bar'                              //  <-- Android ONLY:  HTTP POST params sent to your server when persisting locations.
-        },
-        headers: {                                   // <-- Android ONLY:  Optional HTTP headers sent to your configured #url when persisting locations
-            "X-Foo": "BAR"
-        },
-        desiredAccuracy: 10,
-        stationaryRadius: 20,
-        distanceFilter: 30,
-        notificationTitle: 'Background tracking', // <-- android only, customize the title of the notification
-        notificationText: 'ENABLED', // <-- android only, customize the text of the notification
-        activityType: 'AutomotiveNavigation',
-        debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
-        stopOnTerminate: false // <-- enable this to clear background location settings when the app terminates
-    });
-    bgGeo.start();
-    // bgGeo.stop();
+    ajaxrest.getServerPostition();
     
     // Tamaño container  
     $(".container").css({"min-height":$(document).height()});
