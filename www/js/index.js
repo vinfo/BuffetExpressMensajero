ff9d0a48d0e453887eeb04ab2baa0c5e7ae59288
VICTOR VALENCIA <victor.valencia@gmail.com>
2016-08-09 15:22:56 -0500

    pos

--- www/js/index.js
+++ www/js/index.js
@@ -4,9 +4,13 @@ startApp();
 function startApp() {  
   //alert("startApp");
   localStorage.removeItem("position");
+  localStorage.removeItem("position2");
+  localStorage.removeItem("final_orders");
+  localStorage.removeItem("ordenes");
   localStorage.removeItem("num_ordenes");
   localStorage.removeItem("routes");
   localStorage.removeItem("ruta");
+  localStorage.removeItem("coordinates");
   localStorage.setItem("domain","http://buffetexpress.com.co/REST/");  
   localStorage.setItem("dimension",$(window).width());
   
@@ -19,15 +23,16 @@ function startApp() {
                 lat1= position.coords.latitude;
                 lng1= position.coords.longitude;     
                 localStorage.setItem("position",JSON.stringify({lat:lat1,lng:lng1}));
-				localStorage.setItem("coordinates",lat1+","+lng1);
+                localStorage.setItem("position2",JSON.stringify({lat:lat1,lng:lng1}));
+				        localStorage.setItem("coordinates",lat1+","+lng1);
                 redirect();
               },
               function(error) {
                   alert("Problemas procesando datos.\nNo podemos detectar GEO posición");
-				  console.warn('ERROR(' + error.code + '): ' + error.message);
+				          console.warn('ERROR(' + error.code + '): ' + error.message);
                   //location.reload();
               },
-              {timeout: 40000, enableHighAccuracy: false, maximumAge: 75000}
+              {timeout:15000, enableHighAccuracy:true, maximumAge:0}
       );
   }else{
     alert("Geolocalización no soportada en dispositivo!");
