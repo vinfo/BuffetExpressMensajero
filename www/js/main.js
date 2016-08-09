4da07871bdebf2da66c688d019934a6b7e3129b7
VICTOR VALENCIA <victor.valencia@gmail.com>
2016-08-09 12:55:54 -0500

    asaf

--- www/js/main.js
+++ www/js/main.js
@@ -7,6 +7,10 @@
 	angularRoutingApp.config(function($routeProvider) {
 		$routeProvider
 		.when('/', {
+			templateUrl : 'templates/index.html',
+			controller 	: 'mainController'
+		})		
+		.when('/ordenes', {
 			templateUrl : 'templates/ordenes.html',
 			controller 	: 'ordenesController'
 		})
@@ -18,24 +22,20 @@
 			templateUrl : 'templates/mi_cuenta.html',
 			controller 	: 'mi_cuentaController'
 		})
-		.when('/index', {
-			templateUrl : 'templates/index.html',
-			controller 	: 'mi_cuentaController'
-		})
 		.otherwise({
 			redirectTo: '/'
 		});
 	});
 
-	angularRoutingApp.controller('mainController', function($scope,$location){		
+	angularRoutingApp.controller('mainController', function($scope,$location){
+		localStorage.coordinates='';
 		localStorage.removeItem("flagScreen");
 		if(localStorage.cuenta){
 			$scope.mi_cuenta="#mi_cuenta";
 		}else{
 			$scope.mi_cuenta="login.html";
 			window.location = "login.html";	
-		}
-		
+		}		
 		$scope.closeSession = function () {				
 			localStorage.removeItem("cuenta");
 			$scope.mi_cuenta="login.html";
@@ -55,16 +55,17 @@
 			localStorage.removeItem("flagScreen");
 			$(".latermenu").animate({"left":-412},200);			
 		},
-		$scope.refreshOrders = function () {			
-			RefreshDiv();
+		$scope.refreshOrders = function (){
+			window.location = "internal.html#/ordenes";	
 		}														
 	});		
 
-	angularRoutingApp.controller('ordenesController', function($scope,$location,$interval){		
-		$(".links").attr("href","");					
+	angularRoutingApp.controller('ordenesController', function($scope,$location,$interval){
+		$(".links").attr("href","");			
 		getRoutes();
-		//var timer= $interval(function(){
-			if ( $("#lordenes").length > 0 ) {
+		ajaxrest.getOrders();			
+		var timer= $interval(function(){
+			if ( $("#lordenes").length > 0 ) {				
 				if(localStorage.num_ordenes && !localStorage.flagScreen){
 					var num_orders= JSON.parse(localStorage.num_ordenes);
 					var getOrders = ajaxrest.getOrders();
@@ -72,31 +73,35 @@
 				}else{
 					localStorage.setItem("num_ordenes",JSON.stringify({route:0,num:0}));
 				}
+			}			
+			var pos1= JSON.parse(localStorage.position);
+			var pos2= JSON.parse(localStorage.position2);
+			var p1= pos1["lat"].toString().substring(0,9);
+			var p2= pos2["lat"].toString().substring(0,9);
+			if(p1 !== p2){
+				ajaxrest.setTracking();
+				if(localStorage.position2)localStorage.setItem("position",localStorage.position2);
+				new Maplace().CenterMap();
+				getRoutes();							
 			}
-		//},30000);		
+			ajaxrest.setTracking();			
+		},15000);
 		$(".pedidotar").css({"bottom":$(".menupie").height()+"px"});	
-		localStorage.setItem("request","true");	
+		localStorage.setItem("request","true");
+		//setTimeout(function(){ getOrdens(); }, 5000);	
 		getSummary();
 	});
-
-	angularRoutingApp.controller('programacionController', function($scope,$location,$interval){
+	
+	angularRoutingApp.controller('programacionController', function($scope,$location,$interval){		
 		$(".links").attr("href","internal.html");
 		$(".latermenu").animate({"left":-412},200);
-		var data= ajaxrest.getAgendaDomiciliario();
+		var data= ajaxrest.getAgendaDomiciliario();	
+		//var dat = angular.fromJson(data);
 	});	
 
 	angularRoutingApp.controller('mi_cuentaController', function($scope) {
 		$(".links").attr("href","internal.html");
-		$(".latermenu").animate({"left":-412},200);
-		$scope.changeRoute = function(url, forceReload) {
-			$scope = $scope || angular.element(document).scope();
-			if(forceReload || $scope.$$phase) {
-				window.location = url;
-			} else {
-				$location.path(url);
-				$scope.$apply();
-			}
-		};		
+		$(".latermenu").animate({"left":-412},200);	
 		if(localData!=null && localData!=""){		
 			var data= ajaxrest.getUser("email="+localData['email']+"&token="+localStorage.token);	
 			var dat = angular.fromJson(data);
@@ -105,6 +110,6 @@
 			$scope.lastname = dat[0].lastname;
 			$scope.cellPhone = dat[0].cellPhone;
 		}else{
-			$scope.changeRoute('login.html#/login');
+			window.location = "login.html";	
 		}	
 	});
